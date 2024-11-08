b9d989500dc08d04eb60cd334b11c42afe15e9db
brpetrov <brpetrov@outlook.com>
2024-11-08 18:49:04 +0000

    setup header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login">
-          Something
-        </Button>
+        <Header />
       </div>
       <ButtonGradient />
     </>
