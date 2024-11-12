4ddba2921c9f5f29f22030e59db7fb3e8e6bf1c6
brpetrov <brpetrov@outlook.com>
2024-11-12 19:50:46 +0000

    complete collaboration section

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -3,6 +3,7 @@ import Section from "./Section";
 import { collabApps, collabContent, collabText } from "../constants";
 import { brainwaveSymbol, check } from "../assets";
 import Button from "./Button";
+import { LeftCurve, RightCurve } from "./design/Collaboration";
 
 const Collaboration = () => {
   return (
@@ -31,10 +32,10 @@ const Collaboration = () => {
         </div>
 
         <div className="lg:ml-auto xl:w-[38rem] mt-4">
-          <p className="body-2 mb-4 text-n-4 md:mb-16 lg:mb-32 lg:-w-[22rem] lg:mx-auto">
+          <p className="body-2 mb-10 text-n-4 md:mb-16 lg:mb-32 lg:-w-[22rem] lg:mx-auto">
             {collabText}
           </p>
-          <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
+          <div className=" relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
             <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
               <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
                 <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
@@ -69,6 +70,8 @@ const Collaboration = () => {
                 </li>
               ))}
             </ul>
+            <LeftCurve />
+            <RightCurve />
           </div>
         </div>
       </div>
