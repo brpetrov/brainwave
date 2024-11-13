794c055604da9fe540bdbab35e1599ab0449f753
brpetrov <brpetrov@outlook.com>
2024-11-13 22:21:25 +0000

    Completed the Services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,6 +4,12 @@ import Heading from "./Heading";
 import { service1, service2, service3, check } from "../assets";
 import { brainwaveServices, brainwaveServicesIcons } from "../constants";
 import Generating from "./Generating";
+import {
+  PhotoChatMessage,
+  Gradient,
+  VideoBar,
+  VideoChatMessage,
+} from "./design/Services";
 
 const Services = () => {
   return (
@@ -15,7 +21,7 @@ const Services = () => {
         />
 
         <div className="relative">
-          <div className="relative z-1 flex items-center h-[39rem] mg-b p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
+          <div className="relative z-1 flex items-center h-[39rem] p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem] mb-5">
             <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
               <img
                 className="w-full h-full object-cover md:object-right"
@@ -43,6 +49,77 @@ const Services = () => {
             </div>
             <Generating className="absolute bottom-4 left-4 right-4 border-n-1/10 border lg:left-1/2 lg-right-auto lg:bottom-8 lg:-translate-x-1/2" />
           </div>
+
+          <div className="relative z-1 grid gap-5 lg:grid-cols-2">
+            <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
+              <div className="absolute inset-0">
+                <img
+                  src={service2}
+                  alt="robot"
+                  width={630}
+                  height={750}
+                  className="h-full w-full object-cover"
+                />
+              </div>
+              <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90">
+                <h4 className="h4 mb-4">Photo Editing</h4>
+                <p className="body-2 mb-[3rem] text-n-3">
+                  Automatically enhance your photos using our AI app's photo
+                  editing feature. Try it now!
+                </p>
+              </div>
+              <PhotoChatMessage />
+            </div>
+            <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem]">
+              <div className="py-12 px-5 xl:px-8">
+                <h4 className="h4 mb-4">Video Generation</h4>
+                <p className="body-2 mb-[2rem] text-n-3">
+                  {" "}
+                  The world’s most powerful AI photo and video art generation
+                  engine. What will you create?
+                </p>
+                <ul className="flex items-center justify-between">
+                  {brainwaveServicesIcons.map((icon, index) => (
+                    <li
+                      key={index}
+                      className={`flex items-center justify-center rounded-2xl ${
+                        index === 2
+                          ? "w-[3rem] h-[3rem] p-0.25 bg-conic-gradient  md:w-[4.5rem] md:h-[4.5rem]"
+                          : "flex h-10 w-10 bg-n-6 md:w-15 md:h-15"
+                      }`}>
+                      <div
+                        className={`${
+                          index == 2
+                            ? "flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]"
+                            : ""
+                        }`}>
+                        <img
+                          src={icon}
+                          height={24}
+                          width={24}
+                          alt={icon}
+                          className="object-contain"
+                        />
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="relative h-[20rem] bg-n-8 rounded-xl overflow-hidden md:h-[25rem]">
+                <img
+                  src={service3}
+                  className="w-full h-full object-cover "
+                  width={520}
+                  height={400}
+                  alt="Video generated robot"
+                />
+
+                <VideoChatMessage />
+                <VideoBar />
+              </div>
+            </div>
+          </div>
+          <Gradient />
         </div>
       </div>
     </Section>
