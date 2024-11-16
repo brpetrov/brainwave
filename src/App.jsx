9655d980458cfa4f8373e5849852da080dd0fc76
brpetrov <brpetrov@outlook.com>
2024-11-16 12:40:16 +0000

    complete footer section and build site

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Collaboration from "./components/Collaboration";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
         <Services />
         <Pricing />
         <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
